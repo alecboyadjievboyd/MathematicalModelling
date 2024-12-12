f2a92da07a0114ef546f9f72f5b6ff297dd1061f
Alec Boyadjiev Boyd <a.boyadjiev.boyd@student.tue.nl>
2024-12-12 17:55:43 +0100

    Corrected a little bit of sumn and implemented product pfsf

--- Model/product.py
+++ Model/product.py
@@ -90,7 +90,7 @@ class Product(Expression):
             # Note that the two statments can never trigger at the same time. 
             if (term.expression_type == ExpressionType.PRODUCT): #If one of the terms itself is a product. 
                 popList.append(term) # Add the term to the removal list
-                termsNew = term.consolidate() # Consolidate the inner product (incase there are any more nested products)
+                termsNew = term.consolidate() # Consolidate the inner product (incase there are any more nested products) + to clear zeros from the inner sum
                 for term2 in termsNew.terms:
                     appendList.append(term2) # Add the extracted terms to the append list. 
             elif term == Constant(1):
@@ -102,16 +102,116 @@ class Product(Expression):
         for term in appendList:
             self.factors.append(term)
 
+
+    ## NOTE THAT THIS SHOULD ONLY BE CALLED IF ALL FACTORS ARE IN PFSF FORM ALREADY, MEANING ANY SUMS HAVE AT LEAST TWO TERMS, AT MOST A SINGLE CONSTANT TERM, AND NO NESTED SUMS
+    ## Also pls call it with [] for the factors parameter
+
+    def multOut(self, factors): 
+        if len(self.factors) == 0: # this means everything have been computed and the recursive call has given [] from self.factors[1:]
+            return Product(factors) # returns the product containting all of the factors
+        
+        if self.factors[0].expression_type != ExpressionType.SUM: # if the leading factor isnt a sum
+            factors.append(self.factors[0]) #essenitally leave a running total of the non-sum terms
+            return Product(self.factors[1:]).multOut(factors)
+        
+        else: # we now know it must be a sum
+            newSumTerms = []
+            for term in self.factors[0].terms:
+                factors.append(term)
+                newSumTerms.append(Product(self.factors[1:]).multOut(factors))
+
+                # newSumTerms.append(Product(factors, term, multOut(Product(self.factors[1:]), []))) # Since the trailing set of factors finds its place here, we empty the list to avoid duplication of factors
+
+            return Sum(newSumTerms)
+
     # We now define PFSF
     def pfsf(self):
         
+        from exponential import Exponential
+        
         # first simplify all factors to the max and then consolidate
+
+        factorsPfsf = []
+        for factor in self.factors:
+            factorsPfsf.append(factor.pfsf()) # Simplify all the terms
+
+        factorsPfsf = Product(factorsPfsf).consolidate().factors 
 
         # IF A FACTOR IS A SUM
 
-        # separate the factors that are sums into a different list
-        # multiply these sums, then multiply through the non-sum factors to all
-        # you are now left with a large sum of terms. simply run pfsf on this sum and return that (it will call pfsf again on the terms which are products with no sum factors, thus leading to the next section)
+        sumContained = False
+
+        for factor in factorsPfsf:
+            if factor.expression_type == ExpressionType.SUM: # If any of them are sums (in this case they must be multi-term sums because all factors have been simplified)
+                sumContained = True
+
+        if sumContained == True: # At least one factor is a sum
+
+            return Product(factorsPfsf).multOut([]).pfsf() # Will turn it into a sum and then call pfsf on it, meaning that each internal (sum free) product will then be simplified
+        
+        if sumContained == False:
+
+            constList = []
+
+            for factor in factorsPfsf:
+                if factor.isConstant():
+                    constList.append(factor)
+                elif factor.expression_type != ExpressionType.EXPONENTIAL: # if it is not an exponent (fully expected to be simplified)
+                    factor = Exponential(factor, Constant(1)) # Make it exponent :) 
+
+            for factor in constList: # Removing the constants from the front
+                factorsPfsf.pop(factor)      
+
+            # Now, without constants, all that is left to do is join like terms and order. 
+            orderedFactors = []
+            orderedFactors.append(factorsPfsf[0])      
+
+            # Remember lesser complexity goes first
+            for factor in factorsPfsf[1:]: # all remaining terms
+                added = False
+                for index, factorComp in enumerate(orderedFactors):
+                    if factor.base == factorComp.base: # If the bases are equal
+                        # We worry not about consolidating sums here, as we will pfsfify this at the end
+                        factorComp.argument = Sum(factorComp.argument, factor.argument) # Combine the arguments and join
+                        added = True
+                        break # we no longer need to compare with other terms
+                    elif factor.base < factorComp.base: # This means that it is lesser in complexity than the current entry, which means it goes before it
+                        orderedFactors.insert(index, factor) 
+                        added = True
+                        break # stop comparing because we have found its place
+                    # If it is not <= it is > than the current entry and thus its place is farther to the right, continue the loop
+                # If it has still not been added, append it to the end (it must be < every other term in complexity)
+                if added == False:
+                    orderedFactors.append(factor)
+
+            # orderedFactors list is now ordered as per pfsf
+
+            for factor in orderedFactors:
+
+                if factor.argument.expression_type == ExpressionType.SUM: # If not a sum, it was not joined and thus there is no need to simplify as it is already pfsf (for efficiency)
+                    factor.argument.pfsf() # Simplify the exp
+
+                if factor.argument == Constant(1): # If it is a 1
+                    factor = factor.base # no need for an exponent
+
+                elif term.factors[0] == Constant(0): # If it is a zero
+                    factor = Constant(1) # Exponent resolves to a constant
+                
+                # if neither of these hold, the arg is not zero or one, so we leave it as is
+
+                if len(term.factors) == 1: # only one element left
+                    term = term.factors[0] # remove the product brackets essentially 
+
+            orderedFactors.insert(0, Product(constList).consim()) # Adding the simplified sum of constants to the sum
+            
+            if len(orderedFactors) == 1: # only one term
+                return orderedFactors[0]
+            else:
+                return Sum(orderedFactors)
+        
+
+
+
 
         # IF NO FACTORS ARE SUMS
 
