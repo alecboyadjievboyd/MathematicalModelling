86e18bf302147ed270b658322d612ed5ca0c8589
Mukil-Krishna <m.k.balamurugan@student.tue.nl>
2025-01-11 16:25:37 +0100

    Bug fix on variable parsing

--- parsing.py
+++ parsing.py
@@ -45,7 +45,7 @@ def log(input):
     if (input[0] == '('):
         j += bracket(input)            
     elif (input[0] == 'x'):
-        j += 3
+        j += 2 + const(input[2:])
     else:
         j += const(input)
     
@@ -89,7 +89,7 @@ def factor(input):
             start += bracket(input[start:])
       
         elif (input[start] == 'x'):
-            start += 3            
+            start += 2 + const(input[start+2:])   
 
         elif (input[start] == 's' or input[start] == 't' or input[start] =='c'):
             start += 3
@@ -136,7 +136,7 @@ def unit(input):
     elif (input[0] == '('):
         return expression(input[1:len(input)-1])
     elif (input[0] == 'x'):
-        return Variable(input[2])
+        return Variable(input[2:])
     elif (input[0] == 'e'):
         return  Euler()
     elif (input[0] == 'p'):
@@ -227,75 +227,6 @@ def expression(input):
 # make sure input has no spaces use input = input.replace(" ", "")
 user_input = str(input("please input in ASCII math "))
 user_input = user_input.replace(" ", "")
+
 x = expression(user_input)
-
 print(x)
-
-
-
-""" 
-def factor(input):
-
-    if (input == ""):
-        return
-    
-    if (input[0] == '('):
-        return expression(input)
-    
-    else:
-        l = []
-        j = 0
-        while (j < len(input)): 
-            if (input[j] == 'x'):
-                j+=2
-                a = input[j]
-                if (j+1 < len(input)):
-                    if (input[j+1] == '^'):
-                        j += 3
-                        counter = 0
-                        bracket = 1 
-                        while (bracket > 0):
-                            if (input[j+counter] == '('):
-                                bracket += 1                            
-                            elif(input[j+counter] == ')'):
-                                bracket -= 1
-
-                            counter += 1
-                            
-                        l.append(Exponential(Variable(a), expression(input[j:j+counter])))
-                        j += counter
-                        continue
-                        
-                l.append(Variable(a))
-
-            elif (input[0]<= '9' and input[0] >= '0'):
-                t = const(input)
-                j += t[1]
-
-                if (j+1 < len(input)):
-                    if (input[j+1] == '^'):
-                        j += 3
-                        counter = 0
-                        bracket = 1 
-                        while (bracket > 0):
-                            if (input[j+counter] == '('):
-                                bracket += 1                            
-                            elif(input[j+counter] == ')'):
-                                bracket -= 1
-
-                            counter += 1
-
-                        l.append(Exponential(Constant(t[0]), expression(input[j:j+counter])))
-                        j += counter
-                        continue
-                        
-                l.append(Constant(t[0]))
-                
-            j += 1
-        
-        if (len(l) > 1):
-            return Product(l)
-        else:
-            return l[0] 
-"""
-
