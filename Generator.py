3dc956ba7a130aa888004289da81d08b4d537dc7
Alec Boyadjiev Boyd <a.boyadjiev.boyd@student.tue.nl>
2025-01-15 15:54:26 +0100

    Evaluator works i think

--- Generator.py
+++ Generator.py
@@ -1,4 +1,5 @@
 import random
+import math
 
 from Model.product import Product
 from Model.sum import Sum
@@ -26,7 +27,7 @@ def generate(depth, maxElements):
         if num < 5:
             return Variable(1)
         if num > 7:
-            return Integer(random.randint(0,9))
+            return Integer(random.randint(-9,9))
         if num == 6:
             return Euler()
         if num == 7:
@@ -96,7 +97,141 @@ def generate(depth, maxElements):
 
     elif operation == "Arccosine":
         return Arccosine(generate(random.randint(0, depth - 1), maxElements))  # Replace argument with actual value
+
+def evaluate(f, x):
+
+    if f.expression_type == ExpressionType.INTEGER:
+        return f.value
     
+    if f.expression_type == ExpressionType.EULER:
+        return 2.71828
+
+    if f.expression_type == ExpressionType.PI:
+        return 3.14159
+
+    if f.expression_type == ExpressionType.VARIABLE:
+        return x
+
+    if f.expression_type == ExpressionType.FRACTION: # FRACTIONS ONLY CONTAIN INTEGERS
+        from Model.fraction import Frac
+
+        if f.num == 'undefined' or f.den == 'undefined' or f.den == Integer(0):
+            return 'undefined'
+
+        try:
+            return (f.num / f.den)
+        except Exception:
+            return 'undefined'
+
+    if f.expression_type == ExpressionType.PRODUCT:
+        value = 1
+        for factor in f.factors:
+            ev = evaluate(factor, x)
+            if ev == 'undefined':
+                return 'undefined'
+            value = value * ev
+
+        try:
+            return value
+        except Exception:
+            return 'undefined'
+
+    if f.expression_type == ExpressionType.SUM:
+        value = 0
+        for term in f.terms:
+            ev = evaluate(term, x)
+            if ev == 'undefined':
+                return 'undefined'
+            value = value + ev
+        
+        try:
+            return value
+        except Exception:
+            return 'undefined'
+
+    if f.expression_type == ExpressionType.EXPONENTIAL:
+        baseEv = evaluate(f.base, x)
+        argEv = evaluate(f.argument, x)
+
+        try:
+            return math.pow(baseEv, argEv)
+        except Exception:
+            return 'undefined'
+
+    if f.expression_type == ExpressionType.LOGARITHM:
+        baseEv = evaluate(f.base, x)
+        argEv = evaluate(f.argument, x)
+
+        try:
+            return math.log(argEv, baseEv)
+        except Exception:
+            return 'undefined'
+
+    if f.expression_type == ExpressionType.ARCTANGENT:
+        
+        argEv = evaluate(f.argument, x)
+
+        try:
+            return math.atan(argEv)
+        except Exception:
+            return 'undefined'
+
+    if f.expression_type == ExpressionType.TANGENT:
+        
+        argEv = evaluate(f.argument, x)
+
+        try:
+            return math.tan(argEv)
+        except Exception:
+            return 'undefined'
+
+    if f.expression_type == ExpressionType.SINE:
+        
+        argEv = evaluate(f.argument, x)
+
+        try:
+            return math.sin(argEv)
+        except Exception:
+            return 'undefined'
+
+    if f.expression_type == ExpressionType.ARCSINE:
+        
+        argEv = evaluate(f.argument, x)
+
+        try:
+            return math.asin(argEv)
+        except Exception:
+            return 'undefined'
+
+    if f.expression_type == ExpressionType.COSINE:
+        
+        argEv = evaluate(f.argument, x)
+
+        try:
+            return math.cos(argEv)
+        except Exception:
+            return 'undefined'
+
+    if f.expression_type == ExpressionType.ARCCOSINE:
+        
+        argEv = evaluate(f.argument, x)
+
+        try:
+            return math.acos(argEv)
+        except Exception:
+            return 'undefined'
+
+    else: 
+        print("WHY THE FUCK")
+    
+
+
+
+
+
+
+
+
 count = 0
 while True:
     obj = generate(3,3)
@@ -104,14 +239,17 @@ while True:
     
     try:
         print("NOW TESTING: " + str(obj))
-        print("Simplified safe mode: " + str(obj.pfsf(True)))
-        print("Simplified not safe mode: " + str(obj.pfsf()))
+        print(evaluate(obj, 1))
+        '''print("Simplified safe mode: " + str(obj.pfsf(True)))
+        print("Simplified not safe mode: " + str(obj.pfsf()))'''
     except RecursionError:
         print("Recursion Error, Skip")
+    except MathError:
+        print("Math Error, This Expression is Not Valid")
     print(" ")
     print(count)
     print(" ")
 
-x1 + cos(((7^pi)^((x1 + 6 + arcsin(1))^7))) + tan(x1)
+(((3 + x1 + x1)^log_(arctan(x1))(x1))^(-3))
 
-((0 * x1)^((x1 * (8 * 6 * pi))^x1))
+
